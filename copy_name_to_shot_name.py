8f4880e55b33b31ee4643015a2887911862f5adf
Kieran Hanrahan <kieran@knhn.me>
2022-11-01 17:25:25 -0400

    Add output
    Add shell output for title, version, and as names are copied.

--- copy_name_to_shot_name.py
+++ copy_name_to_shot_name.py
@@ -1,11 +1,13 @@
+#!/usr/bin/env python
+
 """
 Copy Segment Name to Shot Name
 
 URL:
+
     https://github.com/khanrahan/copy-name-to-shot-name
 
 Description:
-
 
     Simple script to copy segment name to shot name on all selected segments.
 
@@ -22,17 +24,29 @@ To Install:
     /opt/Autodesk/user/<user name>/python
 """
 
-
 from __future__ import print_function
 
+__title__ = "Copy Segment to Shot Name"
+__version_info__ = (0, 0, 2)
+__version__ = ".".join([str(num) for num in __version_info__])
+__version_title__ = "{} v{}".format(__title__, __version__)
 
-VERSION = (0, 0, 1)
+MESSAGE_PREFIX = "[PYTHON HOOK]"
+
+def message(string):
+    """Print message to shell window and append global MESSAGE_PREFIX."""
+
+    print(" ".join([MESSAGE_PREFIX, string]))
 
 
 def copy_segment_name_to_shot_name(selection):
 
+    message(__version_title__)
+    message("Script called from {}".format(__file__))
+
     for segment in selection:
         segment.shot_name = segment.name
+        message("{} copied to shot name.".format(segment.name.get_value()))
 
 
 def scope_timeline_clip(selection):
